8b0f9392aa0422e9e783c7db111cd85e2c6506d1
Oğuzhan AYDIN <ogzhnaydin06@gmail.com>
2025-11-04 13:34:06 +0300

    started to script

--- seesaw.js
+++ seesaw.js
@@ -1,2 +1,22 @@
-const canvas = document.getElementById("seesaw-canvas");
-const ctx = canvas.getContext("2d");
+const plank = document.getElementById('plank');
+
+let nextWeight = generateRandomWeight();
+
+function generateRandomWeight() {
+    return  (Math.random() * (10 - 1) + 1).toFixed(1);
+}
+
+function getObjectSize(weight) {
+    return 30 + (weight - 1) * 4;
+}
+
+plank.addEventListener('click', (e) => {
+    const weight = parseFloat(nextWeight);
+
+    const obj = {
+        weight: weight,
+        size: getObjectSize(weight)
+    };
+
+    console.log(obj);
+});
